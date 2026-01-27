5729adbf0337ff73f28c80a6260aa71130c53867
Travis McGhee <travismcghee@Traviss-MacBook-Pro.local>
2026-01-27 16:36:08 -0500

    Cleanup unused files and update config

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "TravisMcGhee";
+export const SITE_TITLE = "Travis McGhee";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
-export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "TM";
+  "IT Graduate exploring Cybersecurity. I write about digital privacy, online safety, and the ever-evolving tech landscape.";
+export const TWITTER_HANDLE = "@travismcghee";
+export const MY_NAME = "Travis McGhee";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
