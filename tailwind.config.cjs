e5e2411a7b50ae5bf7371a07bec9a7dbfd3cb48a
nottrav <plain.boxy@gmail.com>
2026-01-28 09:45:52 -0500

    Switch font from Open Sans to Nunito

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        body: ["Open Sans", "sans-serif"],
-        heading: ["Open Sans", "sans-serif"],
+        body: ["Nunito", "sans-serif"],
+        heading: ["Nunito", "sans-serif"],
       },
       colors: {
         transparent: "transparent",
